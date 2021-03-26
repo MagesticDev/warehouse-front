ab70f2536bd68894651cb2c7ffa834738e12535c
Guyon Stewen <guyonstewen@gmail.com>
2021-03-26 18:29:52 +0100

    add slider + back

--- src/app/core/modeles/slider.model.ts
+++ src/app/core/modeles/slider.model.ts
@@ -2,12 +2,23 @@ export interface ISlider {
     url?: string;
     title?: string;
     description?:string;
+    btn_link?:string;
+    btn_text?:string;
+    upload_date?: Date;
+    modif_date?: Date;
+    pseudo?: string;
 }
 
 export class Slider implements ISlider {
     constructor(
+        public id?: number,
         public url?: string,
         public title?:string,
-        public description?:string
+        public description?:string,
+        public btn_link?:string,
+        public btn_text?:string,
+        public upload_date?: Date,
+        public modif_date?: Date,
+        public pseudo?: string
     ) {}
 }
